8b33ea92f1b85e179e02d11f282b616caff3c2b3
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-09-22 15:57:20 -0700

    Update

--- starter-code/public/scripts/articleView.js
+++ starter-code/public/scripts/articleView.js
@@ -1,7 +1,7 @@
 'use strict';
 var app = app || {};
 
-// TODO: Wrap the contents of this file, except for the preceding 'use strict' and 'var app...' declararions, in an IIFE.
+// TODO: Wrap the contents of this file, except for the preceding 'use strict' and 'var app...' declarations, in an IIFE.
 // Give the IIFE a parameter called 'module'.
 // At the very end of the code, but still inside the IIFE, attach the 'articleView' object to 'module'.
 // Where the IIFE is invoked, pass in the global 'app' object that is defined above.
