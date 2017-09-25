bb27b9015cd05136ac95ab64c65f2d6b9425acc2
Shannon Dillon <sedillon93@gmail.com>
2017-09-25 12:45:15 -0700

    use .map to populate Article.all array

--- marco-shannon/public/scripts/article.js
+++ marco-shannon/public/scripts/article.js
@@ -38,11 +38,13 @@ var app = app || {};
   Article.loadAll = rows => {
     rows.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
 
-    // TODO: Refactor this forEach code, by using a `.map` call instead, since what we are trying to accomplish
+    // DONE: Refactor this forEach code, by using a `.map` call instead, since what we are trying to accomplish
     // is the transformation of one collection into another. Remember that we can set variables equal to the result of functions. So if we set a variable equal to the result of a .map, it will be our transformed array.
     // There is no need to push to anything.
 
-    Article.all = rows.map(function(ele){new Article(ele)})
+    Article.all = rows.map(function(article){
+      return new Article(article);
+    })
   // OLD forEach():
   //   rawData.forEach(function(ele) {
   //   Article.all.push(new Article(ele));
