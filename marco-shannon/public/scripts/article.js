d311b08881f6d012173fc4d787c686cd7f42135e
Shannon Dillon <sedillon93@gmail.com>
2017-09-25 16:59:00 -0700

    saving changes for later

--- marco-shannon/public/scripts/article.js
+++ marco-shannon/public/scripts/article.js
@@ -42,9 +42,7 @@ var app = app || {};
     // is the transformation of one collection into another. Remember that we can set variables equal to the result of functions. So if we set a variable equal to the result of a .map, it will be our transformed array.
     // There is no need to push to anything.
 
-    Article.all = rows.map(function(article){
-      return new Article(article);
-    })
+    Article.all = rows.map(article => new Article(article));
   // OLD forEach():
   //   rawData.forEach(function(ele) {
   //   Article.all.push(new Article(ele));
